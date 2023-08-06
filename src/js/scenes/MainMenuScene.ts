dd55250d3109ae56456c34f2ff8f1e15550a6f9e
judahpaul16 <judahpaul16@gmail.com>
2023-08-06 02:32:44 -0400

    Added  a 'PreMainMenu' + Other Tweaks

--- src/js/scenes/MainMenuScene.ts
+++ src/js/scenes/MainMenuScene.ts
@@ -2,34 +2,71 @@ import Phaser from 'phaser';
 
 export default class MainMenuScene extends Phaser.Scene {
     private mainMenuBG!: Phaser.GameObjects.Video;
+    private clickCounter: number = 0;
+    private insertCoin!: Phaser.GameObjects.Text;
+    private mainMenuText!: Phaser.GameObjects.Text;
+    private timerEvent: Phaser.Time.TimerEvent | null = null;
 
     constructor() {
         super({ key: 'MainMenuScene' });
     }
 
-    private timerEvent: Phaser.Time.TimerEvent | null = null;
-
     create() {
-        // Set the background
-        this.createBackground();
+        this.createPreBackground();
+        this.input.on('pointerdown', this.handleInput, this);
+        this.input.keyboard!.on('keydown', this.handleInput, this);
+    }
 
-        // Start the music
-        this.sound.play('mainMusic', { loop: true });
-        
-        this.add.text(255, 560, 'Press any key to enter\nthe city...', {
+    private handleInput() {
+        this.clickCounter++;
+        this.insertCoin.destroy();
+        if (this.timerEvent) this.timerEvent.destroy(); // Stop the ellipses animation
+        if (this.clickCounter === 1) {
+            // Set the background and show text
+            this.createBackground();
+            this.sound.play('mainMusic', { loop: true });
+            this.mainMenuText = this.add.text(255, 560, 'Press any key to enter\nthe city...', {
+                fontSize: '20px',
+                color: '#000',
+                fontStyle: 'bold',
+                align: 'center'
+            }).setOrigin(0.5);
+            // Ellipses animation
+            let ellipses = '';
+            this.timerEvent = this.time.addEvent({
+                delay: 500,
+                callback: () => {
+                    ellipses += '.';
+                    if (ellipses.length > 3) ellipses = '';
+                    this.mainMenuText.setText('Press any key to enter\nthe city' + ellipses);
+                },
+                loop: true
+            });
+        } else if (this.clickCounter === 2) {
+            // Start the next scene
+            this.scene.start('GameScene1');
+            this.game.registry.set('previousScene', this.scene.key);
+        }
+    }
+
+    private createPreBackground() {
+        // Center
+        this.insertCoin = this.add.text(this.cameras.main.centerX, this.cameras.main.centerY + 50, 'Insert Coin', {
             fontSize: '20px',
-            color: '#000',
-            fontStyle: 'bold',
+            color: '#fff',
             align: 'center'
         }).setOrigin(0.5);
 
-        this.input.on('pointerdown', () => {
-            this.scene.start('GameScene1');
-            this.game.registry.set('previousScene', this.scene.key);
-        });
-        this.input.keyboard!.on('keydown', () => {
-            this.scene.start('GameScene1');
-            this.game.registry.set('previousScene', this.scene.key);
+        // Ellipses animation
+        let ellipses = '';
+        this.timerEvent = this.time.addEvent({
+            delay: 500,
+            callback: () => {
+                ellipses += '.';
+                if (ellipses.length > 3) ellipses = '';
+                this.insertCoin?.setText('Insert Coin' + ellipses);
+            },
+            loop: true
         });
     }
 
@@ -52,5 +89,5 @@ export default class MainMenuScene extends Phaser.Scene {
     
         // Play the video
         this.mainMenuBG.play();
-    }    
+    }
 }
