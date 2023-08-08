c7d53d6bd54ceeb0973e0af2595e6654c7787c41
Judah Paul <judahpaul16@gmail.com>
2023-08-08 00:19:43 -0400

    Renamed some things, added some things

--- src/js/scenes/BootScene.ts
+++ src/js/scenes/BootScene.ts
@@ -33,7 +33,7 @@ import foreground2 from '../../assets/sprites/backgrounds/foreground-2.png';
 import foregroundEmpty from '../../assets/sprites/backgrounds/foreground-empty.png';
 import cyberpunkStreet from '../../assets/sprites/backgrounds/cyberpunk-street.png';
 
-import mainMusicSrc from '../../assets/audio/music/mainMusic.mp3';
+import mainMusicSrc from '../../assets/audio/music/main.mp3';
 
 import p2Dialogue1 from '../../assets/audio/dialogue/player2_1.mp3';
 
