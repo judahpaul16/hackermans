dff8566bd958adc1a2ffde5709109d03c452d482
judahpaul16 <judahpaul16@gmail.com>
2023-11-03 15:52:44 -0400

    Refactored bufferzone compute function

--- src/js/classes/characters/Player.ts
+++ src/js/classes/characters/Player.ts
@@ -263,19 +263,19 @@ export default class Player extends Phaser.Physics.Arcade.Sprite {
     }
 
     private computeBufferZone(followingPlayerNumber: number, activePlayerNumber: number): number {
-        if (followingPlayerNumber === 2 && activePlayerNumber === 1) {
-            return 600;
-        } else if (followingPlayerNumber === 3 && activePlayerNumber === 1) {
-            return 300;
-        } else if (followingPlayerNumber === 1 && activePlayerNumber === 2) {
-            return 600;
-        } else if (followingPlayerNumber === 3 && activePlayerNumber === 2) {
-            return 300;
-        } else if (followingPlayerNumber === 1 && activePlayerNumber === 3) {
-            return 300;
+        switch (`${followingPlayerNumber}-${activePlayerNumber}`) {
+            case '2-1':
+            case '1-2':
+            case '1-3':
+                return 600;
+            case '3-1':
+            case '3-2':
+            case '2-3':
+                return 300;
+            default:
+                return 600;
         }
-        return 600;
-    }
+    }    
 
     public follow(playerToBeFollowed: any) {
         const activePlayer: Player = this.scene.game.registry.get('activePlayer');
